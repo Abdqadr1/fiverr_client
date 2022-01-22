3e7defcede2b33fefd34dfe119b082b1f91b4491
Abdqadr1 <abolarinwaquadri@gmail.com>
2022-01-23 00:50:55 +0800

    first commit

---
+++ home.js
@@ -0,0 +1,5 @@
+const filter = document.querySelector(".filter");
+const dropdownContent = document.querySelector(".dropdown-content");
+filter.addEventListener("click", (event) => {
+    // dropdownContent.style.display = "block";
+})
