770d7761a6ffc34fd20e70cae4759f606aadd3cb
Abdqadr1 <abolarinwaquadri@gmail.com>
2022-01-23 10:12:33 +0800

    finishing home

--- home.js
+++ home.js
@@ -1,5 +1,21 @@
-const filter = document.querySelector(".filter");
-const dropdownContent = document.querySelector(".dropdown-content");
-filter.addEventListener("click", (event) => {
-    // dropdownContent.style.display = "block";
+const filterObject = {
+
+}
+const LengthBar = document.querySelector("ul.length-bar");
+const li = LengthBar.querySelectorAll("li");
+li.forEach(element => {
+    element.onclick = event => {
+        const id = element.id;
+        li.forEach(el => {
+            const _id = el.id;
+            if (id === _id) {
+                el.classList.add("active");
+                el.classList.remove("after")
+            } else {
+                el.classList.remove("active");
+                if (_id < id) el.classList.add("after");
+                else el.classList.remove("after")
+            } 
+        })
+    }
 })
